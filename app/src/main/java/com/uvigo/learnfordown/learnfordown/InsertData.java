18076712bcfbd0f9ea0c38dddc7227e6ac1e1ef4
cris_ <cristinagonzalezescudeiro@gmail.com>
2017-03-28 15:52:24 +0200

    sonidos

--- app/src/main/java/com/uvigo/learnfordown/learnfordown/InsertData.java
+++ app/src/main/java/com/uvigo/learnfordown/learnfordown/InsertData.java
@@ -320,9 +320,11 @@ public class InsertData {
         db.insertar_foto("v","ve","directa","vela","Esta es una * perfumada",R.drawable.vela,"otros",2,"bv");
         db.insertar_foto("v","vo","directa","volante","Mi padre cuando conduce mantiene las dos manos en el *",R.drawable.volante,"otros",3,"bv");
 
-        db.insertar_foto("w","wa","directa","waterpolo","El * se juega en el agua",R.drawable.waterpolo,"otro",4,"gu");
 
-        db.insertar_foto("x","xi","directa","xilofono","El * es un instrumento musical",R.drawable.xilofono,"otro",4,"xi");
+        db.insertar_foto("w","wa","directa","waterpolo","El * se juega en el agua",R.drawable.waterpolo,"otro",4,"gu");
+        db.insertar_foto("x","xi","directa","xilófono","El * es un instrumento musical",R.drawable.xilofono,"otro",4,"x");
+        db.insertar_foto("w","wa","directa","waterpolo","Hemos jugado al * en la piscina de casa",R.drawable.waterpolo,"otro",4,"w");
+
 
         db.insertar_foto("y","ye","directa","yegua","La * ha tenido un potro",R.drawable.yegua,"animales",2,"lly");
         db.insertar_foto("y","yo","directa","yogurt","Siempre ceno un * de postre",R.drawable.yogurt,"comida",2,"lly");
